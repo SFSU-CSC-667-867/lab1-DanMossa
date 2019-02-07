125ad79e90205c9373732a0c05f557cf0d6f63bc
Daniel Mossaband <Daniel@Mossaband.com>
2019-02-06 20:26:59 -0800

    Finished functions.js

--- node/functions.js
+++ node/functions.js
@@ -10,10 +10,16 @@ const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+console.log(myArrow(8));
 
 // make a function that returns a function
+const functionFactory = () => () => console.log("This is weird");
+const a = functionFactory();
+a();
+functionFactory()();
